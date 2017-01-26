08909d3b7373e2e0b207abf6d85582be88255f20
David Berg <david.berg@afconsult.com>
2017-01-26 21:23:19 +0100

    Work in progress

--- src/app.js
+++ src/app.js
@@ -9,41 +9,4 @@ window.onload = function() {
   canvasContext.height = parseInt(sketch_style.getPropertyValue('height'));
 
   let canvas = new Canvas(canvasContext);
-
-  let xStart, yStart, xCurrent, yCurrent;
-
-/* Mouse Capturing Work */
-  canvasContext.addEventListener('mousemove', function(e) {
-    xCurrent = e.pageX - this.offsetLeft;
-    yCurrent = e.pageY - this.offsetTop;
-  }, false);
-
-  canvasContext.addEventListener('mousedown', function(e) {
-    xStart = xCurrent;
-    yStart = yCurrent;
-
-    onPaint();
-
-    canvasContext.addEventListener('mousemove', onPaint, false);
-
-  }, false);
-
-  canvasContext.addEventListener('mouseup', function() {
-    canvasContext.removeEventListener('mousemove', onPaint, false);
-  }, false);
-
-  var distance = function(xStart, yStart, xCurrent, yCurrent) {
-    return Math.sqrt(Math.pow(xStart - xCurrent, 2) + Math.pow(yStart - yCurrent, 2));  // Pythagoras
-  }
-
-  var onPaint = function() {
-
-    if (distance(xStart, yStart, xCurrent, yCurrent) < 5) {
-      return;
-    }
-    
-    canvas.drawLine(xStart, yStart, xCurrent, yCurrent);
-    xStart = xCurrent;
-    yStart = yCurrent;
-  };
 };
--- src/canvas.js
+++ src/canvas.js
@@ -2,6 +2,27 @@ module.exports = class Canvas {
     constructor(canvas) {
         this.paintContext = canvas.getContext('2d');
 
+        canvas.addEventListener('mousemove', (function(e) {
+            console.log(e.pageX, e.pageY);
+            this.xCurrent = e.pageX;
+            this.yCurrent = e.pageY;
+        }).bind(this), false);
+
+        canvas.addEventListener('mousedown', (function(e) {
+            this.xStart = this.xCurrent;
+            this.yStart = this.yCurrent;
+
+            this.onPaint();
+
+            canvas.addEventListener('mousemove', this.onPaint.bind(this), false);
+
+        }).bind(this), false);
+
+        canvas.addEventListener('mouseup', (function() {
+            //TODO: This does not seem to work!!!!
+            canvas.removeEventListener('mousemove', this.onPaint, false);
+        }).bind(this), false);
+
         /* Drawing on Paint App */
         this.lineWidth = 5;
         this.lineCap = 'round';
@@ -22,13 +43,30 @@ module.exports = class Canvas {
     }
 
     set color(color) {
-        this.paintContext.strokeStyle = '#' + color;
+        this.paintContext.strokeStyle = color;
+    }
+  
+    onPaint() {
+        if (Canvas.distance(this.xStart, this.yStart, this.xCurrent, this.yCurrent) < 5) {
+            return;
+        }
+        
+        this.drawLine(this.xStart, this.yStart, this.xCurrent, this.yCurrent);
+
+        this.xStart = this.xCurrent;
+        this.yStart = this.yCurrent;
     }
 
     drawLine(x1, y1, x2, y2) {
+        console.log("woop", x1, x2, y1, y2);
+        console.log(this.paintContext);
         this.paintContext.beginPath();
         this.paintContext.moveTo(x1, y1);
         this.paintContext.lineTo(x2, y2);
         this.paintContext.stroke();
+    }
+
+    static distance (xStart, yStart, xCurrent, yCurrent) {
+        return Math.sqrt(Math.pow(xStart - xCurrent, 2) + Math.pow(yStart - yCurrent, 2));  // Pythagoras
     }
 }
